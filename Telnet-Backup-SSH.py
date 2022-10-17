8cc9a94f55d1fec97749f7bd8a266479df2f7d85
Simon8008 <115245804+Simon8008@users.noreply.github.com>
2022-10-17 10:55:38 +0100

    Create Telnet-Backup-SSH.py

---
+++ Telnet-Backup-SSH.py
@@ -0,0 +1,51 @@
+import pexpect
+  #Define variables
+ip_address = '192.168.56.101'
+username = 'cisco'
+password = 'cisco123!'
+
+connectionType = input("Do you want to connect via SSH or Telnet?: ")
+if connectionType == 'Telnet':
+  
+    #Create telnet session
+    session = pexpect.spawn('telnet ' + ip_address, encoding='utf-8',
+                                timeout=20)
+    result = session.expect (['Username:', pexpect.TIMEOUT])
+
+    # Check for error, if exists then display error and exists
+    if result != 0:
+        print('---Failure! creating sessions for: ', ip_address)
+        exit()
+
+    # Session is expecting username, enter details
+    session.sendline(username)
+    result = session.expect (['Password:', pexpect.TIMEOUT])
+
+    #Check for error, if exists then display error and exit
+    if result != 0:
+        print ('--- FAILURE! entering username: ', username)
+        exit()
+
+    #Session is expecting password, enter details
+    session.sendline(password)
+    result = session.expect (['#', pexpect.TIMEOUT])
+
+    # Check for error, if exists then display error and exit
+    if result != 0:
+        print('---FAILURE! entering password: ', password)
+        exit()
+
+    # Display a success message if it works 
+    print('---------------------------------')
+    print('')
+    print('--- Sucess! connecting to: ', ip_address)
+    print('---               Username: ', username)
+    print('---               Password: ', password)
+    print('')
+    print('----------------------------------')
+
+    #Terminate telnet to advice and close session
+    session.sendline('quit')
+    session.close()
+else:
+    print("Option not found.")
